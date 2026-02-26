99b44bd129a9ba6b675c191440e059fb4575b8fe
Tarun Saini <sainitarun7888@gmail.com>
2026-02-26 12:43:19 +0530

    feat - add onClick navigations

--- E-commerce/src/App.tsx
+++ E-commerce/src/App.tsx
@@ -1,13 +1,26 @@
 
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Navbar'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import ContactUs from './Pages/ContactUs'
+import Register from './Pages/Register'
+import Login from './Pages/Login'
 
 function App() {
 
   return (
     <>
       <div>
-< Navbar/>
+        < Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact-us' element={<ContactUs />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
       </div>
     </>
   )
