b91251e202de1031292fa9df9ef7e6890975c441
Tarun Saini <sainitarun7888@gmail.com>
2026-03-19 15:47:54 +0530

    fix: comment unused variables

--- E-commerce/src/pages/Orders.tsx
+++ E-commerce/src/pages/Orders.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import  { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { BsBox } from "react-icons/bs";
 import { useDispatch, useSelector } from "react-redux";
