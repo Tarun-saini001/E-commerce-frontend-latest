402498e5821a0f58edca79b7407ac81d86fa26db
Tarun Saini <sainitarun7888@gmail.com>
2026-03-23 14:00:11 +0530

    fix- add prodctId in addToCart req body

--- E-commerce/src/redux/slices/cartSlice.ts
+++ E-commerce/src/redux/slices/cartSlice.ts
@@ -39,13 +39,13 @@ export const fetchCart = createAsyncThunk("cart/fetch", async () => {
 export const addToCart = createAsyncThunk(
     "cart/add",
     async (product: Product) => {
-        const { _id, title, price, thumbnail, brand, categoryName, category } = product
+        const { _id } = product
         const quantity=1;
         const res = await fetch(`${API}/service/cart/add`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             credentials: "include",
-            body: JSON.stringify({ _id, title, price, thumbnail, brand,categoryName,category, quantity }),
+            body: JSON.stringify({productId: _id,  quantity }),
         });
         const data = await res.json();
         console.log('add to cart res: ', data);
